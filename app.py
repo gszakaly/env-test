94aef4d6fcce05785d95ba7e5f553c799d988274
Gergely Szakaly <gergely.szakaly@ch.ibm.com>
2021-01-03 19:09:12 +0100

    Print environment

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    s = '<html><body>'
+    for name in os.environ:
+        s += name + ': ' + os.environ[name] + '<br>'
+    s += '</body></html>'
+
+    return s
 
 app.run(host='0.0.0.0')
